cdba76288d1ae8e9451c68cdecfe30ed8a0e887a
hoang-danny05 <bajanboss132@gmail.com>
2023-08-09 00:42:44 -0700

    updated gitignore to add testing files, Added test-vacuum-switch.py

---
+++ test/test-vacuum-switch.py
@@ -0,0 +1,26 @@
+from VacuumSwitch import VacuumSwitch
+import mecademicpy.robot as MecademicRobot
+from time import sleep
+import traceback
+# robot = MecademicRobot.Robot()
+# try:
+#   robot.Connect(address="192.168.0.100", enable_synchronous_mode=True, disconnect_on_exception=False)
+#   robot.ActivateAndHome()
+ # except Exception:
+ #   print("oh noes")
+try:
+    print("connected to robit, doing other stuff")
+    sensor = VacuumSwitch()
+
+    print("entering loop")
+    while True:
+        sleep(.5)
+        print("ON" if (sensor.read_state()) else "OFF")
+except Exception:
+    print("error happened")
+    traceback.print_exc()
+except KeyboardInterrupt:
+    print("exited")
+
+# robot.SetValveState(0,0)
+# robot.Disconnect()
