d36041e530759cebe7b25701da2327c8ee7c2139
hoang-danny05 <bajanboss132@gmail.com>
2023-08-14 11:02:40 -0700

    Fixed GPIO bugs, Changed handling of GPIO AssertionErrors, Added more VaccumSwitch methods

--- VacuumSwitch.py
+++ VacuumSwitch.py
@@ -5,28 +5,45 @@ from RPi import GPIO
 # CONNECTIONS:
 # GPIO 4 (PIN 7) <-> OPTOCOUPLER COLLECTOR
 # GND <-> OPTOCOUPLER EMITTER
+# GPIO 17 (PIN 11) <-> Speaker +
+# GND (14) <-> Speaker -
 ############################
 
 class VacuumSwitch:
     def __init__(self):
+        self.setup()
+
+    def setup(self):
         #pin is the BOARD number of the pi
         try:
             GPIO.setmode(GPIO.BOARD)
             GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
             GPIO.setup(11, GPIO.OUT)
         except Exception:
-           print("error setting up the connection")
+            print("error setting up the connection")
 
     def read_state(self): #TRUE for if there's a part ON, FALSE for if there's NO PART
+        self.setup()
         return (not GPIO.input(7))
     
-    def assert_state(self, condition: bool): #TRUE if there should be a part ON
-        if not (self.read_state() == condition):
-            GPIO.output(11, True)
-            input("ERROR: NO PART ON ROBOT, FIX NOW.")
-            GPIO.output(11, False)
-            raise AssertionError("The robot's part state was not expected")
+    def assert_state(self, partOn: bool): #TRUE if there should be a part ON
+        if not (self.read_state() == partOn):
+            try:
+                self.setup()
+                GPIO.output(11, True)
+                input("ERROR: NO PART ON ROBOT, FIX NOW.")
+                GPIO.output(11, False)
+                raise AssertionError("The robot's part state was not expected")
+            except KeyboardInterrupt:
+                raise AssertionError("The robot has been exited out of the error state")
     
+    def start_alarm(self):
+        self.setup()
+        GPIO.output(11, True)
+
+    def stop_alarm(self):
+        self.setup()
+        GPIO.output(11, False)
+
     def cleanup(self):
         GPIO.cleanup()
-        GPIO.output(11, False)
--- main.py
+++ main.py
@@ -33,6 +33,9 @@ except Exception:
 component = SOP16(robot, switch)
 robot.ActivateAndHome()
 
+###################################
+# INPUT HANDLING
+##################################
 try:
     loops = int(sys.argv[1]) if len(sys.argv) > 1 else 1 #default amount of loops is 1, else it is the first argument
 except ValueError:
@@ -42,6 +45,9 @@ except Exception as e:
     traceback.print_exc()
     loops = 1
 
+##################################
+# MAIN ACTIONS
+##################################
 try:
     print("Starting Try loop")
     print(robot.GetJoints())
@@ -49,11 +55,9 @@ try:
         print(f"Starting Loop {i+1}")
         component.pressButton()
         component.grabComp()
-        if switch.read_state() == False:
-            raise Exception('No part is connected to the robot') 
+        switch.assert_state(True)
         component.flux()
-        if switch.read_state() == False:
-            raise Exception('No part is connected to the robot') 
+        switch.assert_state(True)
         # #component.preheat()
         component.solder()
         component.drop()
@@ -62,17 +66,28 @@ except KeyboardInterrupt:
     robot.ClearMotion()
     print("\nManually Exited With CTRL+C")
 except AssertionError:
-    #beep
-    print("No part on when it should have been")
-    input(":")
+    # stop right there buster!!
+    robot.PauseMotion() #works btw
+    robot.ClearMotion()
+    # beep and wait for hooman
+    switch.start_alarm()
+    try:
+        input("Robot missing part, Press any key to exit:")
+    except KeyboardInterrupt:
+        print("Exited with CTRL + C")
+    switch.stop_alarm()
+    print("exited, returning to normal position. ")
 except Exception as e:
     print(f"Unknown Exception \"{e}\" happened, exiting.")
-    print("######## START TRACEBACK")
+    print("######## START TRACEBACK ########")
     traceback.print_exc()
-    print("######## END TRACEBACK")
+    print("######## END TRACEBACK ########")
 else: 
     print("Successfully Exited.")
 
+####################################
+# PROGRAM END
+####################################
 print("Program finished.")
 try:
     robot.ResumeMotion()
@@ -86,5 +101,6 @@ except Exception:
     print("robot interrupted during deactivation.") 
 # robot.DeactivateRobot()
 
+#should ALWAYS execute on exit
 robot.Disconnect()
 switch.cleanup()
